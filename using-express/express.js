b26b988c2961aa50a5259b9706f56532fe09efcc
clairellesage <clairellesage@gmail.com>
2017-01-17 19:40:39 +0000

    added redirect

--- using-express/express.js
+++ using-express/express.js
@@ -5,27 +5,55 @@ var PORT = process.env.PORT || 8080; // default port 8080
 app.set("view engine", "ejs")
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+ "b2xVn2": "http://www.lighthouselabs.ca",
+ "9sm5xK": "http://www.google.com",
 };
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
+function generateRandomString() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 6; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+ console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/", (req, res) => {
+ res.end("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+ res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+ res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", {templateVars : templateVars});
+ let templateVars = { urls: urlDatabase };
+ res.render("urls_index", {templateVars: templateVars});
 });
+
+app.get("/urls:id", (req, res) => {
+ let templateVars = { urls: urlDatabase };
+ res.render("urls_show", {templateVars: templateVars});
+});
+
+app.get("/urls/new", (req, res) => {
+ res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+ urlDatabase[generateRandomString()] = req.body.longURL;
+ res.redirect("urls_index")
+});
+
