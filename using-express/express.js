169dc1d0df8606b97d19fe5e3faf697ccd30b1c7
clairellesage <clairellesage@gmail.com>
2017-01-20 19:07:25 +0000

    user auth 1.0

--- using-express/express.js
+++ using-express/express.js
@@ -14,26 +14,25 @@ const urlDatabase = {
  "9sm5xK": "http://www.google.com",
 };
 
-const users = {}
+const users = {};
 
 function generateRandomString() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
     for( var i=0; i < 6; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
-
     return text;
 }
 
-app.listen(PORT, () => {
- console.log(`Example app listening on port ${PORT}!`);
-});
-
 app.get("/", (req, res) => {
-	let templateVars = {
-		username: req.cookies["username"],
-	};
+ var templateVars = {
+  urls: urlDatabase,
+ }
+ if (req.cookies.userId) {
+   templateVars.email = users[req.cookies.userId].email;
+ } else {
+ 	templateVars.email = null
+ }
  res.end("Hello!");
 });
 
@@ -46,35 +45,32 @@ app.get("/urls.json", (req, res) => {
 // });
 	
 app.get("/urls", (req, res) => {
-	let templateVars = {
-		username: req.cookies["username"],
-		urls: urlDatabase,
-	};
-	//check this
- res.render("urls_index", templateVars);
+console.log(req.cookies.userId)
+console.log(users)
+ //place templateVars initialization outside of if statement since it is required for res.render
+ var templateVars = {
+  urls: urlDatabase,
+ }
+ if (req.cookies.userId) {
+   templateVars.email = users[req.cookies.userId].email;
+ } else {
+ 	templateVars.email = null
+ }
+res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-	let templateVars = {
-username: req.cookies["username"],
-	};
+ var templateVars = {
+  urls: urlDatabase,
+ }
+ 	if (req.cookies.userId) {
+   		templateVars.email = users[req.cookies.userId].email;
+ 	} else {
+ 	templateVars.email = null
+ }
  res.render("urls_new", templateVars);
 });
-
-//when you type in urls/ and then the id, 
-//it will render urls_show.ejs
-app.get("/urls/:id", (req, res) => {
-	let shortURL = req.params.id
- 	let longURL = urlDatabase[shortURL]
-	let templateVars = {
-	username: req.cookies["username"],
-	shortURL : shortURL,
-	longURL : longURL
-		};	
- res.render("urls_show", templateVars);
-});
-
-//post the function generateRandomString	
+	
 app.post("/urls", (req, res) => {
 	/*assigning a shortURL to the longURL entry 
 	and storing it in the database*/
@@ -90,16 +86,6 @@ app.post("/urls/:id/delete", (req, res) => {
 	delete urlDatabase[shortURL] 
 	res.redirect("/urls");
 })
-//urls_show 
-//:id is the shortURL
-//post modifies the corresponding longURL
-//redirects client back to /urls
-app.post("/urls/:id", (req, res) => {
-	let shortURL = req.params.id;
-	let longURL = urlDatabase[shortURL];
-	urlDatabase[shortURL] = req.body.longURL;
-	res.redirect("/urls");
-})
 
 ///shortURLs in the urlDatabase will redirect to their longURLs
 app.get("/u/:shortURL", (req, res) => {re
@@ -112,11 +98,12 @@ app.get("/u/:shortURL", (req, res) => {re
 	}
 });
 
+//when you register, 
 app.get("/register", (req, res) => {
-		let templateVars = {
-	username: req.cookies["username"],
-	password: req.cookies["password"],
-		};	
+	let templateVars = {
+		username: req.cookies["username"],
+		password: req.cookies["password"],
+	};
 	res.render("urls_register", templateVars);
 });
 
@@ -124,40 +111,84 @@ app.post("/register", (req, res) => {
 	let email = req.body.email;
 	let password = req.body.password;
 	let randId = generateRandomString();
-	res.cookie("username", randId)
+  //creates a cookie called userId with the randId
+	res.cookie("userId", randId)
 	if (email === "" || password === "") {
 		res.status(400);
 		res.send("Error 400 : Please fill in all fields.");
 	}
 	for (key in users) {
-		if (users[key].email === req.body.email) {
+		if (users[key].email === email) {
 			res.status(400);
 			res.send("Sorry, that email is already registered!");
 		}
-	}	users[randId] = {id : randId,
-					email : email,
-					password : password};
-	
+	}	
+  users[randId] = {
+          id : randId,
+		email : email,
+		password : password};
 	res.redirect("/urls");
+});
+
+app.get("/urls/login", (req, res) => {
+	let templateVars = {
+		username: req.cookies["username"],
+		password: req.cookies["password"],
+		};	
+	res.render("urls_login", templateVars)
 })
 
-app.get("/login", (req, res) => {
-	res.render("login")
-})
-
-app.post("/login", (req, res) => {
-	let user = req.body.username;
-	res.cookie("username", user);
-	res.redirect("/");
+app.post("/urls/login", (req, res) => {
+	let email = req.body.email;
+	let password = req.body.password;
+	if (email === "" || password === "") {
+		res.status(400);
+		res.send("Error 400 : Please fill in all fields.");
+  	}
+  	Object.keys(users).forEach(id => {
+		if (users[id].email == email) {
+			if (users[id].password == password) {
+				res.cookie("userId",id)
+				res.redirect("/")
+			}
+		}
+	});
+	res.status(403);
+	res.send("Error 403: This user doesn't exist.")
 });
 
 app.post("/logout", (req, res) => {
-	res.clearCookie("username")
+	res.clearCookie("userId")
 	res.redirect("/");
 });
+
+app.get("/urls/:id", (req, res) => {
+	let shortURL = req.params.id
+ 	let longURL = urlDatabase[shortURL]
+	let templateVars = {
+	username: req.cookies["username"],
+	shortURL : shortURL,
+	longURL : longURL
+		};	
+ res.render("urls_show", templateVars);
+});
+
+//urls_show 
+//:id is the shortURL
+//post modifies the corresponding longURL
+//redirects client back to /urls
+app.post("/urls/:id", (req, res) => {
+	let shortURL = req.params.id;
+	let longURL = urlDatabase[shortURL];
+	urlDatabase[shortURL] = req.body.longURL;
+	res.redirect("/urls");
+})
 // Add a route to accept a POST to /login in your Express server.
 
 // We will just track a string value called username using a cookie.
 
 // Use the endpoint to set the cookie parameter called username to the value submitted in the request body via the form.
 
+app.listen(PORT, () => {
+ console.log(`Example app listening on port ${PORT}!`);
+});
