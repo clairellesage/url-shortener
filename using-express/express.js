e9197136bb24a88be7e9432419aaff0df0a76e11
clairellesage <clairellesage@gmail.com>
2017-01-22 19:37:37 +0000

    all user prefs

--- using-express/express.js
+++ using-express/express.js
@@ -3,18 +3,35 @@ const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+//I used bcrptjs because bcrypt wouldn't install!
+const bcrypt = require('bcryptjs');
+const password = "purple-monkey-dinosaur";
+const hashed_password = bcrypt.hashSync(password, 10);
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
  "b2xVn2": "http://www.lighthouselabs.ca",
  "9sm5xK": "http://www.google.com",
 };
 
-const users = {};
+const userDb = { randId : { 
+    id : null,
+	email : null,
+	password : null,
+	urls : {"b2xVn2": "someUserId",
+			"9sm5xK": "someUserId"}
+	}
+};
+
+function allowCookies(req, res, userDb) {
+	if (userDb[req.cookies.userId]) {
+		userDb.randId.email = userDb[req.cookies.userId].email;
+	} 
+	return userDb;
+}
 
 function generateRandomString() {
     var text = "";
@@ -24,73 +41,59 @@ function generateRandomString() {
     return text;
 }
 
+//HOME
 app.get("/", (req, res) => {
- var templateVars = {
-  urls: urlDatabase,
- }
- if (req.cookies.userId) {
-   templateVars.email = users[req.cookies.userId].email;
- } else {
- 	templateVars.email = null
- }
- res.end("Hello!");
+	allowCookies(req, res, userDb);
+ 	res.end("Hello!", userDb);
 });
 
-app.get("/urls.json", (req, res) => {
- res.json(urlDatabase);
-});
-
-// app.get("/hello", (req, res) => {
-//  res.end("<html><body>Hello <b>World</b></body></html>\n");
-// });
-	
+//URLS
 app.get("/urls", (req, res) => {
-console.log(req.cookies.userId)
-console.log(users)
- //place templateVars initialization outside of if statement since it is required for res.render
- var templateVars = {
-  urls: urlDatabase,
- }
- if (req.cookies.userId) {
-   templateVars.email = users[req.cookies.userId].email;
- } else {
- 	templateVars.email = null
- }
-res.render("urls_index", templateVars);
+	//if there's a userId cookie and it's in the db
+	if (req.cookies.userId && userDb[req.cookies.userId]) {
+		var templateVars = {
+			email: userDb[req.cookies.userId].email,
+			urls: userDb[req.cookies.userId].urls
+		}
+		res.render("urls_index", templateVars);
+	} else {
+		res.redirect("/login");
+	}
 });
 
+app.post("/urls", (req, res) => {
+	/*assigning a shortURL to the longURL entry and storing it in the userDb*/
+	let shortURL = generateRandomString();
+	userDb[req.cookies.userId].urls[shortURL] = req.body.longURL
+	res.redirect("/urls")
+});
+
+//NEW
 app.get("/urls/new", (req, res) => {
- var templateVars = {
-  urls: urlDatabase,
- }
- 	if (req.cookies.userId) {
-   		templateVars.email = users[req.cookies.userId].email;
- 	} else {
- 	templateVars.email = null
- }
- res.render("urls_new", templateVars);
+	if (req.cookies.userId && userDb[req.cookies.userId]) {
+		var templateVars = {
+			email: userDb[req.cookies.userId].email,
+			urls: userDb[req.cookies.userId].urls
+		}
+		res.render("urls_new", templateVars);
+	} else {
+		res.redirect("/login")
+	}
 });
 	
-app.post("/urls", (req, res) => {
-	/*assigning a shortURL to the longURL entry 
-	and storing it in the database*/
- urlDatabase[generateRandomString()] = req.body.longURL;
- res.redirect("/urls")
-});
-
-/*will "delete" for any id submitted by the delete button
+/*will delete for any id submitted by the delete button
 in urls_index.ejs, using app.post*/ 
 app.post("/urls/:id/delete", (req, res) => {
-	let shortURL= req.params.id;
+	let shortURL = req.params.id;
 	//deletes shortURL from the database, and in turn the longURL
-	delete urlDatabase[shortURL] 
+	delete userDb[req.cookies.userId].urls[shortURL];
 	res.redirect("/urls");
 })
 
-///shortURLs in the urlDatabase will redirect to their longURLs
-app.get("/u/:shortURL", (req, res) => {re
+///shortURLs in the urlDatabase redirect to their longURLs
+app.get("/u/:shortURL", (req, res) => {
 	let shortURL= req.params.shortURL; 
-  let longURL = urlDatabase[shortURL];
+  let longURL = userDb.urls[shortURL];
   if (shortURL = false) {
   	res.redirect("urls_index");
   } else {
@@ -98,96 +101,99 @@ app.get("/u/:shortURL", (req, res) => {re
 	}
 });
 
-//when you register, 
-app.get("/register", (req, res) => {
-	let templateVars = {
-		username: req.cookies["username"],
-		password: req.cookies["password"],
-	};
-	res.render("urls_register", templateVars);
+//REGISTER
+app.get("/register", (req, res) => {	
+	res.render("urls_register");
 });
 
 app.post("/register", (req, res) => {
-	let email = req.body.email;
-	let password = req.body.password;
 	let randId = generateRandomString();
-  //creates a cookie called userId with the randId
-	res.cookie("userId", randId)
-	if (email === "" || password === "") {
+	userDb[randId] = {};
+	userDb[randId].email = req.body.email;
+	userDb[randId].password = req.body.password;
+	userDb[randId].urls = {};
+
+	const hashed_password = bcrypt.hashSync(password, 10);
+	console.log(hashed_password)
+	res.cookie("userId", randId);
+
+	if (req.body.email === "" || req.body.password === "") {
 		res.status(400);
 		res.send("Error 400 : Please fill in all fields.");
+		//there's probably a problem here
+	} else if (userDb.randId.email === req.body.email) {
+		res.status(400);
+		res.send("Sorry, that email is already registered!");
 	}
-	for (key in users) {
-		if (users[key].email === email) {
-			res.status(400);
-			res.send("Sorry, that email is already registered!");
-		}
-	}	
-  users[randId] = {
-          id : randId,
-		email : email,
-		password : password};
 	res.redirect("/urls");
+
 });
 
-app.get("/urls/login", (req, res) => {
-	let templateVars = {
-		username: req.cookies["username"],
-		password: req.cookies["password"],
-		};	
-	res.render("urls_login", templateVars)
+//LOGIN
+app.get("/login", (req, res) => {
+	//if statement if already logged in
+	if (userDb[req.cookies.userId]) {
+		res.redirect("/urls")
+	}
+	res.render("urls_login", userDb)
 })
 
-app.post("/urls/login", (req, res) => {
-	let email = req.body.email;
-	let password = req.body.password;
+app.post("/login", (req, res) => {
+	var templateVars = {
+		email: req.body.email,
+		password: req.body.password,
+	}
 	if (email === "" || password === "") {
 		res.status(400);
 		res.send("Error 400 : Please fill in all fields.");
   	}
-  	Object.keys(users).forEach(id => {
-		if (users[id].email == email) {
-			if (users[id].password == password) {
-				res.cookie("userId",id)
+  Object.keys(userDb).forEach(id => {
+		if (userDb[id].email == email) {
+			if (bcrypt.compareSync(req.body.password, userDb[id].password)) {
+				res.cookie("userId", userDb[id].id)
 				res.redirect("/")
 			}
-		}
+		} 
 	});
 	res.status(403);
-	res.send("Error 403: This user doesn't exist.")
+	res.send("Error 403: Wrong username or password.")
 });
 
+//LOGOUT
 app.post("/logout", (req, res) => {
-	res.clearCookie("userId")
+	res.clearCookie("userId");
 	res.redirect("/");
 });
 
+//TINY URL
 app.get("/urls/:id", (req, res) => {
+	let randId = req.cookies.userId
 	let shortURL = req.params.id
- 	let longURL = urlDatabase[shortURL]
+	let longURL = userDb[randId].urls[shortURL]
 	let templateVars = {
-	username: req.cookies["username"],
-	shortURL : shortURL,
-	longURL : longURL
-		};	
- res.render("urls_show", templateVars);
+			shortURL : shortURL,
+			longURL : longURL,
+			email: userDb[randId].email,
+		}
+		//allowCookies(req, res, templateVars)
+  res.render("urls_show", templateVars);
 });
 
-//urls_show 
-//:id is the shortURL
 //post modifies the corresponding longURL
-//redirects client back to /urls
 app.post("/urls/:id", (req, res) => {
+	let randId = req.cookies.userId
 	let shortURL = req.params.id;
-	let longURL = urlDatabase[shortURL];
-	urlDatabase[shortURL] = req.body.longURL;
-	res.redirect("/urls");
-})
-// Add a route to accept a POST to /login in your Express server.
-
-// We will just track a string value called username using a cookie.
-
-// Use the endpoint to set the cookie parameter called username to the value submitted in the request body via the form.
+ 	let longURL = req.body.longURL
+ 	userDb[randId].urls[shortURL] = longURL
+ 	let templateVars = {
+	 	email: userDb[randId].email,
+		urls : userDb[randId].urls,
+		shortURL : shortURL,
+		longURL : longURL
+	}
+	//allowCookies(req, res, templateVars)
+  res.render("urls_index", templateVars);
+});
 
 app.listen(PORT, () => {
  console.log(`Example app listening on port ${PORT}!`);
